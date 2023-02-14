b186eb9b057a4c2291288a030bdc93de033f60ac
Demetrios Loizides <loizidesdemetris@gmail.com>
2023-02-14 02:51:31 +0200

    created gallery header component

--- photo-gallery/src/app/Features/photo-gallery/photo-gallery.module.ts
+++ photo-gallery/src/app/Features/photo-gallery/photo-gallery.module.ts
@@ -4,12 +4,14 @@ import { CommonModule } from '@angular/common';
 import { PhotoGalleryRoutingModule } from './photo-gallery-routing.module';
 import { PhotoGalleryComponent } from './photo-gallery.component';
 import { PhotoWidgetComponent } from './components/photo-widget/photo-widget.component';
+import { GalleryHeaderComponent } from './components/gallery-header/gallery-header.component';
 
 
 @NgModule({
   declarations: [
     PhotoGalleryComponent,
-    PhotoWidgetComponent
+    PhotoWidgetComponent,
+    GalleryHeaderComponent
   ],
   imports: [
     CommonModule,
